26f64ce5e8be8a83689eb3538ee75a15f170c3ab
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-09 10:48:33 +0600

    task status added

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -10,11 +10,14 @@ const tasksSlice = createSlice({
     reducers: {
         addTask: (state, {payload}) => {
             if(state.tasks.length === 0){
-                state.tasks.push({id: 1, ...payload});
+                state.tasks.push({id: 1, status: "pending", ...payload});
             }
             else{
-                state.tasks.push({id: state.tasks.at(-1).id + 1, ...payload});
+                state.tasks.push({id: state.tasks.at(-1).id + 1, status: "pending", ...payload});
             }
+        },
+        removeTask: (state, {payload}) => {
+            state.tasks = state.tasks.filter(task => task.id !== payload);
         }
     },
 })
