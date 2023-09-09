4372e0d02f690be467293a52db708c7d301a4444
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-09 14:07:48 +0600

    task delete done

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,6 +1,6 @@
 import { ArrowRightIcon, TrashIcon } from '@heroicons/react/24/outline';
 import { useDispatch } from 'react-redux';
-import { updateTask } from '../../redux/features/tasks/tasksSlice';
+import { removeTask, updateTask } from '../../redux/features/tasks/tasksSlice';
 
 const TaskCard = ({task}) => {
   // const task = {
@@ -30,7 +30,7 @@ const TaskCard = ({task}) => {
       <div className="flex justify-between mt-3">
         <p>{task?.date}</p>
         <div className="flex gap-3">
-          <button title="Delete">
+          <button onClick={()=> dispatch(removeTask(task.id))} title="Delete">
             <TrashIcon className="h-5 w-5 text-red-500" />
           </button>
           <button
