4c1303206bda002525e8d6ccc59b019b4a2f72e2
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-12 19:48:36 +0600

    mytask list added

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -1,38 +1,40 @@
 import {
   CheckIcon,
   DocumentMagnifyingGlassIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserTask } from "../../redux/features/tasks/tasksSlice";
 
 const MyTasks = () => {
-  const item = {
-    id: 1,
-    status: 'pending',
-    title: 'Remove Button',
-    description:
-      'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
-    date: '2023-08-28',
-    assignedTo: 'Mir Hussain',
-    priority: 'high',
-  };
+  const { tasks, userTask } = useSelector((state) => state.tasksSlice);
+  const { name } = useSelector((state) => state.userSlice);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setUserTask(name));
+  }, [tasks, name, dispatch]);
 
   return (
     <div>
       <h1 className="text-xl my-3">My Tasks</h1>
       <div className=" h-[750px] overflow-auto space-y-3">
-        <div
-          key={item.id}
-          className="bg-secondary/10 rounded-md p-3 flex justify-between"
-        >
-          <h1>{item.title}</h1>
-          <div className="flex gap-3">
-            <button className="grid place-content-center" title="Details">
-              <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
-            </button>
-            <button className="grid place-content-center" title="Done">
-              <CheckIcon className="w-5 h-5 text-primary" />
-            </button>
+        {userTask.map((task) => (
+          <div
+            key={task.id}
+            className="bg-secondary/10 rounded-md p-3 flex justify-between"
+          >
+            <h1>{task.title}</h1>
+            <div className="flex gap-3">
+              <button className="grid place-content-center" title="Details">
+                <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
+              </button>
+              <button className="grid place-content-center" title="Done">
+                <CheckIcon className="w-5 h-5 text-primary" />
+              </button>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     tasks: [
@@ -7,42 +7,46 @@ const initialState = {
             status: 'pending',
             title: 'Remove Button',
             description:
-              'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+                'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
             date: '2023-08-28',
             assignedTo: 'Mir Hussain',
             priority: 'high',
-          }
+        }
     ],
+    userTask: [],
 }
 
 const tasksSlice = createSlice({
     name: 'tasksSlice',
     initialState,
     reducers: {
-        addTask: (state, {payload}) => {
-            if(state.tasks.length === 0){
-                state.tasks.push({id: 1, status: "pending", ...payload});
+        addTask: (state, { payload }) => {
+            if (state.tasks.length === 0) {
+                state.tasks.push({ id: 1, status: "pending", ...payload });
             }
-            else{
-                state.tasks.push({id: state.tasks.at(-1).id + 1, status: "pending", ...payload});
+            else {
+                state.tasks.push({ id: state.tasks.at(-1).id + 1, status: "pending", ...payload });
             }
         },
-        removeTask: (state, {payload}) => {
+        removeTask: (state, { payload }) => {
             state.tasks = state.tasks.filter(task => task.id !== payload);
         },
-        updateTask: (state, {payload}) => {
+        updateTask: (state, { payload }) => {
             const targetTask = state.tasks.find(task => task.id === payload);
-            if(targetTask.status === "pending"){
+            if (targetTask.status === "pending") {
                 targetTask.status = "running";
             }
-            else if(targetTask.status === "running"){
+            else if (targetTask.status === "running") {
                 targetTask.status = "completed";
             }
-            else{
+            else {
                 targetTask.status = "archived";
             }
+        },
+        setUserTask: (state, { payload }) => {
+            state.userTask = state.tasks.filter(task => task.assignedTo === payload);
         }
     },
 })
-export const {addTask, removeTask, updateTask} = tasksSlice.actions;
+export const { addTask, removeTask, updateTask, setUserTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
