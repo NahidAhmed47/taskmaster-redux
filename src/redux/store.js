6bd47e58b90e0b0a703b8ea1f9313a1a1d5ee169
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-12 19:33:53 +0600

    user slice created

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksSlice from './features/tasks/tasksSlice';
+import userSlice from './features/users/userSlice';
 
 const store = configureStore({
     reducer: {
         tasksSlice: tasksSlice,
+        userSlice: userSlice,
     },
 });
 
